7a5fac46bfb18e6ffdcc078f39dea366993bc8d6
Roua Labbaoui <rlabbaou@student.42berlin.de>
2026-03-31 19:20:22 +0200

    fixed the execution of h and utils files

--- get_next_line.h
+++ get_next_line.h
@@ -23,5 +23,6 @@ char	*get_next_line(int fd);
 char	*ft_strlcpy(char *dst, char *src, size_t size);
 int		ft_strchr(char *s, int c);
 char	*ft_strjoin(char *s1, char *s2);
-
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *s);
 #endif
